d32372b2c68e01924e96fdfffdbd56f674f41192
Sunday Abam jnr <60232994+SundayAbamjnr@users.noreply.github.com>
2020-06-03 12:26:48 +0100

    Add files via upload
    Run with coderunner or on the terminal

---
+++ index.py
@@ -0,0 +1,7 @@
+
+Name = 'Sunday Abam jnr'
+id = 'HNG-01 425'
+Lang = 'python'
+Email = 'abamsunday5@gmail.com'
+Output = 'Hello World,This is {} with {} using {} for task 2  with  Email {}'
+print(Output.format(Name, id, Lang, Email))
