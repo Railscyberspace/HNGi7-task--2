f82fdf97e56a5f4a1edb3467869b5d90bedd19b6
Sunday Abam jnr <60232994+SundayAbamjnr@users.noreply.github.com>
2020-06-05 07:00:36 +0100

    Add files via upload

---
+++ SundayAbamJnr.py
@@ -0,0 +1,7 @@
+
+Name = 'SundayAbamjnr'
+id = 'HNG-01425'
+Email = 'abamsunday5@gmail.com'
+lang = 'Python'
+Output = 'Hello World,This is {} with {} with email {} using {} for task 2 '
+print(Output.format(Name, id, Email, lang))
