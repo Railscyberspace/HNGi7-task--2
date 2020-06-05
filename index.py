0a90e3a3ef158323645764feb935237a3672fa70
Sunday Abam jnr <60232994+SundayAbamjnr@users.noreply.github.com>
2020-06-05 06:59:53 +0100

    Delete index.py

--- index.py
+++
@@ -1,7 +0,0 @@
-
-Name = 'Sunday Abam jnr'
-id = 'HNG-01 425'
-Lang = 'python'
-Email = 'abamsunday5@gmail.com'
-Output = 'Hello World,This is {} with {} using {} for task 2  with  Email {}'
-print(Output.format(Name, id, Lang, Email))
